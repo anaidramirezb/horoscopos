d73d9f43fb246d1edb6be8d3cecb32d2773779b8
Saúl García <saulg.garciam@gmail.com>
2019-08-24 20:39:59 -0700

    Get data

--- script.js
+++ script.js
@@ -1,66 +1,48 @@
-// Get the cards
-const aries = document.getElementById('aries')
-const tauro = document.getElementById('tauro')
-const cancer = document.getElementById('cancer')
-const leo = document.getElementById('leo')
-const virgo = document.getElementById('virgo')
-const acuario = document.getElementById('acuario')
-const piscis = document.getElementById('piscis')
-const capricornio = document.getElementById('capricornio')
-const sagitario = document.getElementById('sagitario')
-const escorpio = document.getElementById('escorpio')
-const libra = document.getElementById('libra')
-const gemini = document.getElementById('gemini')
-
-// Get API data
-const API_URL = 'https://api.adderou.cl/tyaas/'
-const opts = {
-  crossDomain: true
-}
-function obtenerPersonaje(id) {
-  return new Promise((resolve, reject) => {
-    const url = `${API_URL}`
-    $
-      .get(url, opts, function(data) {
-        resolve(data)
-      })
-      .fail(() => reject())
-  })
-}
-
-this.signos = {
-  aries,
-  tauro,
-  cancer,
-  virgo,
-  leo,
-  acuario,
-  piscis,
-  sagitario,
-  capricornio,
-  escorpio,
-  libra,
-  gemini
-}
-
-
-function clicka () {
-  document.getElementById("aries").addEventListener("click", this.getSign);
-  document.getElementById("tauro").addEventListener("click", this.getSign);
-  document.getElementById("cancer").addEventListener("click", this.getSign);
-  document.getElementById("virgo").addEventListener("click", this.getSign);
-  document.getElementById("leo").addEventListener("click", this.getSign);
-  document.getElementById("acuario").addEventListener("click", this.getSign);
-  document.getElementById("piscis").addEventListener("click", this.getSign);
-  document.getElementById("sagitario").addEventListener("click", this.getSign);
-  document.getElementById("capricornio").addEventListener("click", this.getSign);
-  document.getElementById("escorpio").addEventListener("click", this.getSign);
-  document.getElementById("libra").addEventListener("click", this.getSign);
-  document.getElementById("gemini").addEventListener("click", this.getSign);
-
-}
-function getSign(){
-  alert('djksadjla')
-}
-
-clicka()
+(async function load() {
+  // Get the cards
+  const aries = document.querySelector("#aries");
+  const tauro = document.querySelector("#tauro");
+  const cancer = document.querySelector("#cancer");
+  const leo = document.querySelector("#leo");
+  const virgo = document.querySelector("#virgo");
+  const acuario = document.querySelector("#acuario");
+  const piscis = document.querySelector("#piscis");
+  const capricornio = document.querySelector("#capricornio");
+  const sagitario = document.querySelector("#sagitario");
+  const escorpio = document.querySelector("#escorpio");
+  const libra = document.querySelector("#libra");
+  const gemini = document.querySelector("#gemini");
+  let tarots = [];
+  // Get API data
+  const API_URL = "https://api.adderou.cl/tyaas/";
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  tarots = data.horoscopo;
+  console.log(tarots);
+  addEventClick(aries, "aries");
+  addEventClick(tauro, "tauro");
+  addEventClick(cancer, "cancer");
+  addEventClick(leo, "leo");
+  addEventClick(virgo, "virgo");
+  addEventClick(acuario, "acuario");
+  addEventClick(capricornio, "capricornio");
+  addEventClick(sagitario, "sagitario");
+  addEventClick(escorpio, "escorpio");
+  addEventClick(libra, "libra");
+  addEventClick(gemini, "gemini");
+  addEventClick(piscis, "piscis");
+  function addEventClick($element, sign) {
+    $element.addEventListener("click", () => {
+      let nombre = tarots[sign].nombre;
+      let amor = tarots[sign].amor;
+      let color = tarots[sign].color;
+      let dinero = tarots[sign].dinero;
+      let fechaSigno = tarots[sign].fechaSigno;
+      let numero = tarots[sign].numero;
+      let salud = tarots[sign].salud;
+      alert(
+        `Amor: ${amor}\nDinero: ${dinero}\nSalud: ${salud}\nFechas del Signo: ${fechaSigno}\nColor: ${color}\nNumero de la suerte: ${numero}\n `
+      );
+    });
+  }
+})();
